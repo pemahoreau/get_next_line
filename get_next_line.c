215340ac9a18c1811ba730a6b3e8e194b1eddec7
pemahoreau <pema.horeau@gmail.com>
2017-05-01 15:09:52 -0700

    added main to test

--- get_next_line.c
+++ get_next_line.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -13,30 +12,46 @@
 
 #include "get_next_line.h"
 
-=======
-#include "libft.h"
-
-int		get_next_line(const int fd, char **line)
+static int		get_line()
 {
-	char	buf[BUFFSIZE + 1];
+	int		*tmp;
+
+	tmp = ;
+	return (0);
+}
+
+int				get_next_line(const int fd, char **line)
+{
+	char	buf[BUFF_SIZE + 1];
 	int		nbr_read;
 
-	while (nbr_read = read(fd, buf, BUFFSIZE))
+	while (nbr_read = read(fd, buf, BUFF_SIZE))
 	{
+		if (fd < 0)
+			return (0);
 		buf[nbr_read] = '\0';
-		
+
 	}
 }
 
 
 
-	while ((nbr_read = read(fd, buf, 20)) > 0)
-	{
-		buf[nbr_read] = '\0';
-		tmp = malloc(sizeof(char) * (ft_strlen(buf) + ft_strlen(result) + 1));
-		ft_strcpy(tmp, result);
-		ft_strcat(tmp, buf);
-		free(result);
-		result = tmp;
-	}
->>>>>>> d163cbe152bb26e2ed459ecc458ab1e998bd4f68
+// while ((nbr_read = read(fd, buf, 20)) > 0)
+// {
+// 	buf[nbr_read] = '\0';
+// 	tmp = malloc(sizeof(char) * (ft_strlen(buf) + ft_strlen(result) + 1));
+// 	ft_strcpy(tmp, result);
+// 	ft_strcat(tmp, buf);
+// 	free(result);
+// 	result = tmp;
+// }
+
+
+
+int		main(void) 
+{ 
+	char	*line;
+
+	get_next_line(0, &line); 
+	return (0); 
+}
--- get_next_line.h
+++ get_next_line.h
@@ -12,7 +12,12 @@
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
 # include "libft/libft.h"
+
+# define BUF_SIZE 1024
 
 int		get_next_line(const int fd, char **line);
 
